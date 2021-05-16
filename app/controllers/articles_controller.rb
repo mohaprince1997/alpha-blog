d23a3b56f61d9ad36c4faf7cd892199b5b5625f5
Mohamed Berkache <mohaprince1997@gmail.com>
2021-05-16 03:02:33 +0100

    add edit and lists and show and edit home page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,56 @@
 class ArticlesController < ApplicationController
+    before_action :find_article,only: [:show,:edit,:update,:destroy]
+
     def new
         @article=Article.new
     end
+
+
     def create
-        @article=Article.new(article_params)
-        @article.save
+        @article = Article.new(article_params)
+       if @article.save
+        flash[:notice] = "Article was successfully added in the database"
+        redirect_to article_path(@article)
+       else
+        #redirect_to new_article_path
+        render "new"
+       end
+    end
+
+
+    def show
+       
+    end
+
+    def edit
+      
+    end
+    
+    def update
+    
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated in the database"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def destroy
+       
+        @article.destroy
+        flash[:notice] = "Article was succssefully destroyed"
+        redirect_to articles_path
     end
 
     private
+    def find_article
+        @article = Article.find(params[:id])
+    end
     def article_params
         params.require(:article).permit(:title,:description)
     end
