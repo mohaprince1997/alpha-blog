167ab2ca8dd73067bf6e4d68b56109dcf1df7cf7
mohaprince1997 <moha-prince@hotmail.fr>
2021-06-22 14:37:00 +0200

    show view and edit article show add created by user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
-  def gravatar_for(user,options={size: 80})
+  def gravatar_for(user,options={size: 80,classes: ""})
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
+    classes = options[:classes]
     gravatar_url= "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url,class: "rounded-circle")
+    image_tag(gravatar_url,class: "rounded-circle #{classes}")
     
   end
 end
