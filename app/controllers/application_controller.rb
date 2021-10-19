c7cbc5ff34b3a741d189709661d9f9c4f7292f2a
mohaprince1997 <83657987+mohaprince1997@users.noreply.github.com>
2021-10-19 14:17:22 +0200

    Add session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,19 @@ class ApplicationController < ActionController::Base
 
 
   def logged_in?
-
+    !!current_user
   end
 
   def require_login
+    if !logged_in?
+      flash[:danger] = "You need to login"
+      redirect_to root_path
+
+    end
   end
 
   def current_user
-    @current_user ||= User.find
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 end
